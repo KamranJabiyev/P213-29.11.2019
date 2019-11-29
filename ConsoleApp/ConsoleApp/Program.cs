413f2b8fdd6f466ef9c8bf55370020ae7c03fdfb
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2019-11-28 21:01:37 -0800

    Add files via upload

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variables
+            //string name = "Memmed";
+            //name = "Elgiz";
+            //int number = 6;
+            //bool isMarried = 1 > 0;
+            //decimal x = 3.1M;
+            //int y = int.Parse(name);
+            //Console.WriteLine(name);
+            //Console.ReadKey();
+            #endregion
+
+            //int age = 29;
+
+            //if (age > 16)
+            //{
+            //    Console.WriteLine("Xosh Gelmishsiniz");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Get sonra gelersen");
+            //}
+
+            //switch (age)
+            //{
+            //    case 16:
+            //        Console.WriteLine("Get sonra gelersen");
+            //        break;
+            //    case 18:
+            //        Console.WriteLine("Xosh Gelmishsiniz");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Neche yashun var ala senun?");
+            //        break;
+            //}
+            //string name = "Memmed";
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            //foreach (char letter in name)
+            //{
+            //    Console.WriteLine(letter);
+            //}
+
+        }
+    }
+}
